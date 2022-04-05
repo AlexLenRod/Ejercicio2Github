4aa20d99fb10800f69e324c5817f6a1b390829a0
ALLER <ALLER@DESKTOP-LJAEDA7.home>
2022-04-05 05:36:02 +0200

    Cuarto Commit codigo modificado se ha agregado el metodo division

--- Ejercicio2Github/src/operaciones.java
+++ Ejercicio2Github/src/operaciones.java
@@ -5,7 +5,7 @@
  */
 import java.util.Scanner; 
 /**
- * La idea de esta app es que el usuario inicialmente ingresando un numero de entre 1 y 3 seleccione
+ * La idea de esta app es que el usuario inicialmente ingresando un numero de entre 1 y 4 seleccione
  * la operacion que desea realizar, una vez hace su eleccion se le piden dos numeros que seran los
  * parametros que recibiran los metodos que realizan dichas operaciones. Finalmente se imprime por
  * consola el resultado de la operacion.
@@ -20,7 +20,7 @@ public class operaciones {
 		 int opcion;
 	     double numero1, numero2, resultado=0;
 
-	     /* Aqui se despliega un menu donde se le pide al usuario que haga una eleccion entre tres 
+	     /* Aqui se despliega un menu donde se le pide al usuario que haga una eleccion entre cuatro 
 	      * operaciones
 	      */
 	     System.out.println("C A L C U L A D O R A");
@@ -29,6 +29,7 @@ public class operaciones {
 	     System.out.println("1.-Suma");
 	     System.out.println("2.-Resta");
 	     System.out.println("3.-Multiplicacion");
+	     System.out.println("4.-Division");
 
 
 	     System.out.print("ingrese numero de operacion quieres realizar : ");      
@@ -39,7 +40,7 @@ public class operaciones {
 	      * Comprobacion de que el numero ingresado por el usuario si se encuentre entre el rango 
 	      * establecido
 	      */
-	     if (opcion >=1 && opcion <=3)
+	     if (opcion >=1 && opcion <=4)
 	     {
 	    	/**
 	    	 * Se pide al usuario que ingrese dos numeros que seran los operandos de la operacion
@@ -84,6 +85,18 @@ public class operaciones {
 	           //Se llama al metodo multip pasandole los dos numeros ingresados como parametros
 	           multip(numero1, numero2); 
 	           resultado = multip(numero1, numero2);
+	        }
+	        
+	        /**
+	         * Si el usuario elige la opcion 4, se realiza una division
+	         */
+	        if (opcion==4)
+	        {
+
+	           System.out.println("Usted a elegido dividir los dos numeros");
+	           //Se llama al metodo division pasandole los dos numeros ingresados como parametros
+	           division(numero1, numero2); 
+	           resultado = division(numero1, numero2);
 	        }
 	        
 	        //Se imprime por consola un texto mas el resultado de la operacion realizada
@@ -136,5 +149,18 @@ public class operaciones {
     	double r3 = a * b;
     	return r3;
     }
+
+	/**
+	 * Metodo division que recibe dos parametros y devuelve el valor que resulta de dividirlos
+	 *  
+	 * @param a. Este parametro sera el primer operando de la division
+	 * @param b. Este parametro sera el segundo operando de la division
+	 * @return. Se devuelve el resultado de dividir los dos parametros recibidos
+	 */
+    public static double division(double a, double b) {
+    	double r1 = a / b;
+    	return r1;
+    }
+
 
 }
