7e71652eb962eaf98c0f6cfe8a58e374bda10dde
ALLER <ALLER@DESKTOP-LJAEDA7.home>
2022-04-05 04:38:43 +0200

    Segundo Commit se ha comentado una parte del codigo

--- Ejercicio2Github/src/operaciones.java
+++ Ejercicio2Github/src/operaciones.java
@@ -1,15 +1,28 @@
 
 
-
+/**
+ * Se importa la utilidad de Scanner para poder ingresar datos desde teclado
+ */
 import java.util.Scanner; 
-
+/**
+ * La idea de esta app es que el usuario inicialmente ingresando un numero de entre 1 y 3 seleccione
+ * la operacion que desea realizar, una vez hace su eleccion se le piden dos numeros que seran los
+ * parametros que recibiran las funciones que realizan dichas operaciones. Finalmente se imprime por
+ * pantalla el resultado de la operacion.
+ *  
+ * @author ALLER
+ *
+ */
 public class operaciones {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int opc;
+		// Declaracion de variables
+		 int opc;
 	     double num1, num2, result=0;
 
+	     /* Aqui se despliega un menu donde se le pide al usuario que haga una eleccion entre tres 
+	      * operaciones
+	      */
 	     System.out.println("C A L C U L A D O R A");
 	     Scanner entrada = new Scanner(System.in);
 
@@ -22,16 +35,21 @@ public class operaciones {
 	     opc = entrada.nextInt();
 	     System.out.print("\n");
 	       
-
+	     /**
+	      * Comprobacion de que el numero ingresado por el usuario si se encuentre entre el rango 
+	      * establecido
+	      */
 	     if (opc >=1 && opc <=3)
 	     {
-
+	    	//Se pide al usuario que ingrese dos numeros que seran los operandos de la operacion
 	        System.out.print("ingrese primer numero : ");
 	        num1 = entrada.nextDouble();
 	        System.out.print("ingrese segundo numero : ");
 	        num2 = entrada.nextDouble();
 	          
-
+	        /**
+	         * Si el usuario elige la opcion 1, se realiza una suma
+	         */
 	        if (opc==1)
 	        {
 
