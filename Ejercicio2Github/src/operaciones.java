71c133e00c4fe914b339b1a26e4c94d4203127e7
ALLER <ALLER@DESKTOP-LJAEDA7.home>
2022-04-05 05:04:38 +0200

    Segundo Commit se ha terminado de hacer los comentarios al codigo

--- Ejercicio2Github/src/operaciones.java
+++ Ejercicio2Github/src/operaciones.java
@@ -7,8 +7,8 @@ import java.util.Scanner;
 /**
  * La idea de esta app es que el usuario inicialmente ingresando un numero de entre 1 y 3 seleccione
  * la operacion que desea realizar, una vez hace su eleccion se le piden dos numeros que seran los
- * parametros que recibiran las funciones que realizan dichas operaciones. Finalmente se imprime por
- * pantalla el resultado de la operacion.
+ * parametros que recibiran los metodos que realizan dichas operaciones. Finalmente se imprime por
+ * consola el resultado de la operacion.
  *  
  * @author ALLER
  *
@@ -41,7 +41,10 @@ public class operaciones {
 	      */
 	     if (opc >=1 && opc <=3)
 	     {
-	    	//Se pide al usuario que ingrese dos numeros que seran los operandos de la operacion
+	    	/**
+	    	 * Se pide al usuario que ingrese dos numeros que seran los operandos de la operacion
+	    	 * que el metodo correspondiente recibira como parametros
+	    	 */
 	        System.out.print("ingrese primer numero : ");
 	        num1 = entrada.nextDouble();
 	        System.out.print("ingrese segundo numero : ");
@@ -55,55 +58,83 @@ public class operaciones {
 
 	           System.out.println("Usted a elegido sumar los dos numeros");
 	           suma(num1, num2);
+	           //Se llama al metodo suma pasandole los dos numeros ingresados como parametros
 	           result = suma(num1, num2);
 	        }
 	          
-
+	        /**
+	         * Si el usuario elige la opcion , se realiza una resta
+	         */
 	        if (opc==2)
 	        {
 
 	           System.out.println("Usted a elegido restar los dos numeros");
+	           //Se llama al metodo resta pasandole los dos numeros ingresados como parametros
 	           resta(num1, num2);
 	           result = resta(num1, num2);
 	        }
 	          
-
+	        /**
+	         * Si el usuario elige la opcion 3, se realiza una multiplicacion
+	         */
 	        if (opc==3)
 	        {
 
 	           System.out.println("Usted a elegido multiplicar los dos numeros");
+	           //Se llama al metodo multip pasandole los dos numeros ingresados como parametros
 	           multip(num1, num2); 
 	           result = multip(num1, num2);
 	        }
 	        
-
+	        //Se imprime por consola un texto mas el resultado de la operacion realizada
 	        System.out.print("El resultado de la operacion es : "+result);
 	        System.out.print("\n");
 	     }
 	     
 	     else
-
+	    	/**
+	    	 * Si el usuario no ha ingresado uno de los numeros que se corresponde con una de 
+	    	 * las operaciones, se mostrara un mensaje en consola indicando el error
+	    	 */
 	        System.out.println("el numero "+opc+ " no corresponde a ninguna operacion");
 	        System.out.print("\n");
 	}
 
+	/**
+	 * Metodo suma que recibe dos parametros y devuelve el valor que resulta de sumarlos
+	 *  
+	 * @param a. Este parametro sera el primer operando de la suma
+	 * @param b. Este parametro sera el segundo operando de la suma
+	 * @return. Se devuelve el resultado de sumar los dos parametros recibidos
+	 */
+    public static double suma(double a, double b) {
+    	double r1 = a + b;
+    	return r1;
+    }
 
-   public static double suma(double a, double b) {
-     double r1 = a + b;
-     return r1;
-   }
+    /**
+	 * Metodo resta que recibe dos parametros y devuelve el valor que resulta de restarlos
+	 *  
+	 * @param a. Este parametro sera el primer operando de la resta
+	 * @param b. Este parametro sera el segundo operando de la resta
+	 * @return. Se devuelve el resultado de restar los dos parametros recibidos
+	 */
+    public static double resta(double a, double b) {
+    	double r2 = a - b;
+    	return r2;
+    }
 
    
-   public static double resta(double a, double b) {
-     double r2 = a - b;
-     return r2;
-   }
-
-   
-   
-   public static double multip(double a, double b) {
-     double r3 = a * b;
-     return r3;
-   }
+    /**
+	 * Metodo multip que recibe dos parametros y devuelve el valor que resulta de multiplicarlos
+	 *  
+	 * @param a. Este parametro sera el primer operando de la multiplicacion
+	 * @param b. Este parametro sera el segundo operando de la multiplicacion
+	 * @return. Se devuelve el resultado de sumar los dos parametros recibidos
+	 */
+    public static double multip(double a, double b) {
+    	double r3 = a * b;
+    	return r3;
+    }
 
 }
